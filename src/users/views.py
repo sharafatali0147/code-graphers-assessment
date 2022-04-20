e2c5c499f83636c7f3be99219f9faddf78a02736
Ali <sharafatali0147@gmail.com>
2022-04-21 01:30:11 +0500

    update

--- src/users/views.py
+++ src/users/views.py
@@ -3,13 +3,15 @@ from rest_framework.permissions import AllowAny
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
+from src.services.getIP import get_client_ip
 from src.services.ipgeolocation import IPGeolocationService
 from src.users.models import User
 from src.users.permissions import IsUserOrReadOnly
 from src.users.serializers import CreateUserSerializer, UserSerializer
 
 
-class UserViewSet(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet):
+
+class UserViewSet(viewsets.ModelViewSet):
     """
     Creates, Updates and Retrieves - User Accounts
     """
@@ -17,26 +19,28 @@ class UserViewSet(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.Cre
     queryset = User.objects.all()
     serializers = {'default': UserSerializer, 'create': CreateUserSerializer}
     permissions = {'default': (IsUserOrReadOnly,), 'create': (AllowAny,)}
+    
+    def create(self, request):
+        data = request.data
+        data['username'] =  data['email']
+        serialzer = UserSerializer(data=data)
+        if serialzer.is_valid(raise_exception=True):
+            pass
+            # serialzer.save()
+        else:
+            return Response(status=401)    
+        print('='*45)
+        print(get_client_ip(self.request))
+        print('='*45)
+        return Response(serialzer.data, status=status.HTTP_201_CREATED)
 
     
     def get_serializer_class(self):
-        print('='*45)
-        print(get_client_ip(self.request))
-        print('='*45)
-        print("get_serializer_class")
         return self.serializers.get(self.action, self.serializers['default'])
 
     def get_permissions(self):
-        print('='*45)
-        print(get_client_ip(self.request))
-        print('='*45)
-        
         self.permission_classes = self.permissions.get(self.action, self.permissions['default'])
-        user = super().get_permissions()
-        import pprint
-        pprint.pprint(user)
-        # print(user[0].get('pk'))
-        return user
+        return super().get_permissions()
 
     @action(detail=False, methods=['get'], url_path='me', url_name='me')
     def get_user_data(self, instance):
@@ -46,10 +50,3 @@ class UserViewSet(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.Cre
             return Response({'error': 'Wrong auth token' + e}, status=status.HTTP_400_BAD_REQUEST)
 
 
-def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
